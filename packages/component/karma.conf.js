6933a61b592b6de44f50b53ce316e660e5096849
dblue <dblue@deloitte.com>
2019-11-06 11:45:39 -0500

    chore(component): cleanup karma config

--- packages/component/karma.conf.js
+++ packages/component/karma.conf.js
@@ -1,11 +1,15 @@
-const path = require('path');
-
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-spec-reporter'),
+      require('karma-webpack')
+    ],
     files: ['src/test.ts'],
     preprocessors: {
       'src/test.ts': ['webpack']
@@ -13,24 +17,10 @@ module.exports = function(config) {
     webpack: {
       mode: 'development',
       module: {
-        rules: [
-          {
-            test: /\.ts?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/
-          }
-        ]
+        rules: [{ test: /\.ts?$/, use: 'ts-loader', exclude: /node_modules/ }]
       },
-      resolve: {
-        extensions: ['.ts', '.js']
-      },
-      output: {
-        filename: '[name].bundle.js',
-        chunkFilename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-      }
+      resolve: { extensions: ['.ts', '.js'] }
     },
-    exclude: [],
     reporters: ['spec'],
     port: 9876,
     colors: true,
