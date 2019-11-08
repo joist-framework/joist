fa8e6f343d351b04d25048d2891df447bfdec814
dblue <dblue@deloitte.com>
2019-11-08 16:50:03 -0500

    test(component): add more application tests

--- packages/component/src/lib/app.spec.ts
+++ packages/component/src/lib/app.spec.ts
@@ -1,13 +1,36 @@
-import { Injector } from '@lit-kit/di';
+import { Injector, Service, Inject } from '@lit-kit/di';
 
 import { bootstrapApplication, ROOT_INJECTOR } from './app';
+import { Component } from './component';
+import { html } from 'lit-html';
+import { createComponent } from './create-component';
 
 describe('app', () => {
   it('should create a global Injector instance', () => {
-    expect(ROOT_INJECTOR).toBeUndefined();
-
     bootstrapApplication();
 
     expect(ROOT_INJECTOR instanceof Injector).toBe(true);
+  });
+
+  it('should use the root injector when creating components', () => {
+    bootstrapApplication();
+
+    @Service()
+    class MyService {}
+
+    @Component({
+      tag: 'my-component',
+      defaultState: undefined,
+      template() {
+        return html``;
+      }
+    })
+    class MyComponent {
+      constructor(@Inject(MyService) public myService: MyService) {}
+    }
+
+    const el = createComponent(MyComponent);
+
+    expect(el.componentInstance.myService).toBe(ROOT_INJECTOR!.get(MyService));
   });
 });
