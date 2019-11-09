b41329f884e77bd5a25fa7e3b41c7d2ed1f913f3
dblue <dblue@deloitte.com>
2019-11-09 09:31:24 -0500

    feat(schematics): add application schematic

--- packages/schematics/src/application/index.ts
+++ packages/schematics/src/application/index.ts
@@ -1,9 +1,36 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, url, apply, move, template, mergeWith, chain } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
-export function application(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+export interface ApplicationOptions {
+  name: string;
+}
+
+export function application(options: ApplicationOptions): Rule {
+  return () => {
+    return chain([
+      (tree, context) => {
+        const sourceTemplates = url('./files');
+        const completedTemplates = apply(sourceTemplates, [
+          template(options),
+          move('.', options.name)
+        ]);
+
+        return mergeWith(completedTemplates)(tree, context);
+      },
+      tree => {
+        const name = options.name;
+
+        tree.rename(`./${name}/.gitignore.template`, `./${name}/.gitignore`);
+        tree.rename(`./${name}/karma.conf.js.template`, `./${name}/karma.conf.js`);
+        tree.rename(`./${name}/package.json.template`, `./${name}/package.json`);
+        tree.rename(`./${name}/webpack.config.js.template`, `./${name}/webpack.config.js`);
+        tree.rename(`./${name}/workbox-config.js.template`, `./${name}/workbox-config.js`);
+
+        return tree;
+      },
+      (_, context) => {
+        context.addTask(new NodePackageInstallTask(options.name));
+      }
+    ]);
   };
 }
--- packages/schematics/src/application/index_spec.ts
+++ packages/schematics/src/application/index_spec.ts
@@ -8,8 +8,34 @@ describe('new-application', () => {
   it('works', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
 
-    const tree = runner.runSchematic('application', {}, Tree.empty());
+    const tree = runner.runSchematic('application', { name: 'hello-world' }, Tree.empty());
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files.sort()).toEqual([
+      '/hello-world/.gitignore',
+      '/hello-world/.prettierrc',
+      '/hello-world/README.md',
+      '/hello-world/karma.conf.js',
+      '/hello-world/package.json',
+      '/hello-world/src/app/app.component.ts',
+      '/hello-world/src/assets/.gitkeep',
+      '/hello-world/src/assets/icons/apple-touch-icon-180.png',
+      '/hello-world/src/assets/icons/favicon.ico',
+      '/hello-world/src/assets/icons/icon-144.png',
+      '/hello-world/src/assets/icons/icon-192.png',
+      '/hello-world/src/assets/icons/icon-256.png',
+      '/hello-world/src/assets/icons/icon-36.png',
+      '/hello-world/src/assets/icons/icon-384.png',
+      '/hello-world/src/assets/icons/icon-48.png',
+      '/hello-world/src/assets/icons/icon-512.png',
+      '/hello-world/src/assets/icons/icon-72.png',
+      '/hello-world/src/assets/icons/icon-96.png',
+      '/hello-world/src/index.html',
+      '/hello-world/src/main.ts',
+      '/hello-world/src/manifest.json',
+      '/hello-world/src/test.ts',
+      '/hello-world/tsconfig.json',
+      '/hello-world/webpack.config.js',
+      '/hello-world/workbox-config.js'
+    ]);
   });
 });
