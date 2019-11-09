20c5736ed889898e23fe58c91fd572e5ba963a85
dblue <dblue@deloitte.com>
2019-11-09 15:39:09 -0500

    chore(component): add bootstrap default args

--- packages/component/src/lib/app.ts
+++ packages/component/src/lib/app.ts
@@ -2,6 +2,6 @@ import { Injector, Provider } from '@lit-kit/di';
 
 export let ROOT_INJECTOR: Injector | undefined;
 
-export const bootstrapApplication = (providers: Provider<any>[]) => {
+export const bootstrapApplication = (providers: Provider<any>[] = []) => {
   ROOT_INJECTOR = new Injector({ providers });
 };
