84383b3ac5958aea2278f37a1cee5956cb138acd
dblue <dblue@deloitte.com>
2019-11-10 08:44:43 -0500

    feat(schematics): update generated app.component.ts

--- packages/schematics/src/application/files/src/app/app.component.ts
+++ packages/schematics/src/application/files/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@lit-kit/component';
 import { html } from 'lit-html';
 
-export interface AppState {}
+export interface AppState {
+  title: string;
+}
 
 @Component<AppState>({
   tag: 'app-root',
-  defaultState: {},
+  defaultState: { title: '<%= name %>' },
   style: html`
     <style>
       :host {
@@ -17,9 +19,9 @@ export interface AppState {}
       }
     </style>
   `,
-  template(_state, _run) {
+  template(state, _run) {
     return html`
-      <h1>Hello World</h1>
+      <h1>${state.title}</h1>
     `;
   }
 })
