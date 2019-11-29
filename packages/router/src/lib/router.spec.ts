9871686ffd7201ec3283e3fec0f5280242664178
Danny Blue <dannybluedesign@gmail.com>
2019-11-28 22:01:38 -0500

    stuff

--- packages/router/src/lib/router.spec.ts
+++ packages/router/src/lib/router.spec.ts
@@ -1,8 +1,32 @@
-import { Router } from './router';
+import { Component } from '@lit-kit/component';
+
+import { Router } from './router2';
 
 describe('Router', () => {
   it('should work', () => {
-    // const router = new Router([]);
+    @Component({
+      tag: 'c-one',
+      initialState: null,
+      template() {
+        return '';
+      }
+    })
+    class One {}
+
+    const router = new Router();
+
+    router.registerRoutes([
+      {
+        path: '/products\\/(.*)\\/edit\\/(.*)/',
+        component: () => One
+      }
+    ]);
+
+    window.history.pushState(null, 'title', '/products/10/edit/20');
+
+    router.resolve().then(res => {
+      console.log(res);
+    });
 
     expect(Router).toBeTruthy();
   });
