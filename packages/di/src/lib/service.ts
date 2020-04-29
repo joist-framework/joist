265e856305ffd9cac09fe31a59c427a114a4b554
dblue <dblue@deloitte.com>
2020-04-29 11:21:36 -0400

    refactor(di): simplify provideInRoot

--- packages/di/src/lib/service.ts
+++ packages/di/src/lib/service.ts
@@ -1,12 +1,8 @@
 import { ProviderToken } from './provider';
 import { getMetadataRef } from './metadata';
 
-export interface ServiceConfig {
-  provideInRoot: boolean;
-}
-
-export function Service(serviceConfig: ServiceConfig = { provideInRoot: true }) {
-  return function(provider: ProviderToken<any>) {
-    getMetadataRef(provider).provideInRoot = serviceConfig.provideInRoot;
+export function Service() {
+  return function (provider: ProviderToken<any>) {
+    getMetadataRef(provider).provideInRoot = true;
   };
 }
