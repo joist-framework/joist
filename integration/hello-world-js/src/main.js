ddc1c087bb972a4feeba4065f9716cacebe51912
dblue <dblue@deloitte.com>
2020-04-30 09:32:52 -0400

    chore(integration): update vanilla js example

--- integration/hello-world-js/src/main.js
+++ integration/hello-world-js/src/main.js
@@ -3,12 +3,19 @@ import { Component, defineElement, State } from '@joist/component';
 const AppElement = defineElement(
   Component({
     initialState: '',
-    template({ state }) {
-      const title = document.createElement('h1');
+    template({ state, run }) {
+      const div = document.createElement('div');
 
+      const title = document.createElement('h1');
       title.innerHTML = state;
 
-      return ul;
+      const button = document.createElement('button');
+      button.innerHTML = 'Click Me!';
+      button.addEventListener('click', run('TEST_HANDER'));
+
+      div.append(title, button);
+
+      return div;
     },
   })(
     class {
@@ -20,12 +27,26 @@ const AppElement = defineElement(
         return [State];
       }
 
+      static get handlers() {
+        return {
+          TEST_HANDER: ['foo', 'bar'],
+        };
+      }
+
       constructor(state) {
         this.state = state;
       }
 
       connectedCallback() {
         this.state.setValue('Hello World');
+      }
+
+      foo() {
+        console.log('FOO CALLED');
+      }
+
+      bar() {
+        console.log('BAR CALLED');
       }
     }
   )
