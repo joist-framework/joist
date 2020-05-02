5a7471ced25e3933a73add7f5faf90783be8ed1d
dblue <dblue@deloitte.com>
2020-05-02 19:29:48 -0400

    fix(schematics): remove unused files

--- integration/hello-world-js/src/main.js
+++ integration/hello-world-js/src/main.js
@@ -1,55 +1,55 @@
-import { Component, defineElement, State } from '@joist/component';
+import { defineElement, State } from '@joist/component';
 
-const AppElement = defineElement(
-  Component({
-    state: '',
-    render({ state, run }) {
-      const div = document.createElement('div');
+class AppComponent {
+  static get componentDef() {
+    return {
+      state: '',
+      render({ state, run }) {
+        const div = document.createElement('div');
 
-      const title = document.createElement('h1');
-      title.innerHTML = state;
+        const title = document.createElement('h1');
+        title.innerHTML = state;
 
-      const button = document.createElement('button');
-      button.innerHTML = 'Click Me!';
-      button.addEventListener('click', run('TEST_HANDLER'));
+        const button = document.createElement('button');
+        button.innerHTML = 'Click Me!';
+        button.addEventListener('click', run('TEST_HANDLER'));
 
-      div.append(title, button);
+        div.append(title, button);
 
-      return div;
-    },
-  })(
-    class AppComponent {
-      static get props() {
-        return [];
-      }
+        return div;
+      },
+    };
+  }
 
-      static get deps() {
-        return [State];
-      }
+  static get props() {
+    return [];
+  }
 
-      static get handlers() {
-        return {
-          TEST_HANDLER: ['foo', 'bar'],
-        };
-      }
+  static get handlers() {
+    return {
+      TEST_HANDLER: ['foo', 'bar'],
+    };
+  }
 
-      constructor(state) {
-        this.state = state;
-      }
+  static get deps() {
+    return [State];
+  }
 
-      connectedCallback() {
-        this.state.setValue('Hello World');
-      }
+  constructor(state) {
+    this.state = state;
+  }
 
-      foo() {
-        console.log('FOO CALLED');
-      }
+  connectedCallback() {
+    this.state.setValue('Hello World');
+  }
 
-      bar() {
-        console.log('BAR CALLED');
-      }
-    }
-  )
-);
+  foo() {
+    console.log('FOO CALLED');
+  }
 
-customElements.define('app-root', AppElement);
+  bar() {
+    console.log('BAR CALLED');
+  }
+}
+
+customElements.define('app-root', defineElement(AppComponent));
