33f1c5fe2ba7b6e690365716411e9be956e27d7f
dblue <dblue@deloitte.com>
2020-07-30 11:23:30 -0400

    feat(component): add optional validators to propertoes

--- packages/component/lib/property.ts
+++ packages/component/lib/property.ts
@@ -1,12 +1,22 @@
 import { PropChange } from './lifecycle';
 
-export function property() {
+export type PropValidator = (val: any) => boolean;
+
+export function property(...validators: PropValidator[]) {
   return function (target: any, key: string) {
     const valueKey = `__prop__${key}__value`;
     const initKey = `__prop__${key}__initialized`;
 
     Object.defineProperty(target, key, {
       set(val) {
+        if (validators.length) {
+          validators.forEach((validator) => {
+            if (!validator(val)) {
+              throw new Error(`Validation failed when assigning ${val} to key ${key}`);
+            }
+          });
+        }
+
         if (this.onPropChanges) {
           const oldValue = this[key];
 
--- packages/component/lib/propety.spec.ts
+++ packages/component/lib/propety.spec.ts
@@ -25,4 +25,32 @@ describe('property', () => {
       new PropChange('hello', 'Goodbye World', false, 'Hello World'),
     ]);
   });
+
+  it('should throw and error if the validtor returns false', () => {
+    class MyElement {
+      @property((val) => typeof val === 'string')
+      public hello: any = 'Hello World';
+    }
+
+    const el = new MyElement();
+
+    expect(() => (el.hello = 0)).toThrowError(`Validation failed when assigning 0 to key hello`);
+  });
+
+  it('should throw and error if ALL validators do not return true', () => {
+    const isString = (val: unknown) => typeof val === 'string';
+    const isLongerThan = (length: number) => (val: string) => val.length > length;
+
+    class MyElement {
+      @property(isString, isLongerThan(2))
+      public hello: any = 'Hello World';
+    }
+
+    const el = new MyElement();
+
+    expect(() => (el.hello = 0)).toThrowError(`Validation failed when assigning 0 to key hello`);
+    expect(() => (el.hello = 'He')).toThrowError(
+      `Validation failed when assigning He to key hello`
+    );
+  });
 });
