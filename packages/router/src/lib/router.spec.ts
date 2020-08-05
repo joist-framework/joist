97dcc7063957a3dae0f9b68f343c77d17ad132fe
dblue <dblue@deloitte.com>
2020-08-05 14:30:25 -0400

    test(router): add test for route changes

--- packages/router/src/lib/router.spec.ts
+++ packages/router/src/lib/router.spec.ts
@@ -22,4 +22,34 @@ describe('Router', () => {
 
     expect(router.getFragment()).toBe('foo/bar');
   });
+
+  it('should listen for navigation changes', (done) => {
+    class MockLocation extends Location {
+      private path = '/foo/bar';
+
+      getPath() {
+        return this.path;
+      }
+
+      goTo(path: string) {
+        this.path = path;
+      }
+
+      onPopState() {
+        return () => {};
+      }
+    }
+
+    const router = new Router(new MockLocation(), new RouterConfig());
+
+    const removeListener = router.listen(() => {
+      removeListener();
+
+      expect(router.getFragment()).toBe('foo/bar');
+
+      done();
+    });
+
+    router.navigate('/foo/bar/');
+  });
 });
