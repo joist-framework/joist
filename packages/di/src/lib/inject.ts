1fb9a52b64b62e382da27ae3c99d3e079f2e09e7
dblue <dblue@deloitte.com>
2020-08-07 13:52:28 -0400

    fix(di): try for babel again

--- packages/di/src/lib/inject.ts
+++ packages/di/src/lib/inject.ts
@@ -3,14 +3,12 @@ import { ProviderToken } from './provider';
 export const PROVIDER_DEPS_KEY = 'deps';
 
 export function getProviderDeps(provider: ProviderToken<any>): ProviderToken<any>[] {
-  return provider[PROVIDER_DEPS_KEY] || [];
+  return provider[PROVIDER_DEPS_KEY] || provider.prototype[PROVIDER_DEPS_KEY] || [];
 }
 
 export function inject(injectable: ProviderToken<any>) {
   return function (target: any, _: string, index: number) {
-    const provider = target.constructor || target;
-
-    provider[PROVIDER_DEPS_KEY] = provider[PROVIDER_DEPS_KEY] || [];
-    provider[PROVIDER_DEPS_KEY][index] = injectable;
+    target[PROVIDER_DEPS_KEY] = target[PROVIDER_DEPS_KEY] || [];
+    target[PROVIDER_DEPS_KEY][index] = injectable;
   };
 }
