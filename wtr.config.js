81ca66fb054afaabfdf1e8969b8638a18c163bdc
dblue <dblue@deloitte.com>
2020-09-02 12:09:45 -0400

    ci: use puppeteer

--- wtr.config.js
+++ wtr.config.js
@@ -2,6 +2,7 @@ const { esbuildPlugin } = require('@web/dev-server-esbuild');
 
 module.exports = {
   rootDir: __dirname,
+  puppeteer: true,
   nodeResolve: true,
   plugins: [esbuildPlugin({ ts: true })],
 };
