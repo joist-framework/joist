5e91f38e0ebb37be3cb40016b9ac02cc6e834b79
dblue <dblue@deloitte.com>
2021-11-21 23:31:46 -0500

    publish next

--- packages/component/lib/property.ts
+++ packages/component/lib/property.ts
@@ -2,49 +2,14 @@ export class PropChange<T = any> {
   constructor(public key: string | symbol, public newValue: T, public oldValue?: T) {}
 }
 
-export interface OnPropChanges {
-  onPropChanges(changes: Map<string, PropChange>): void;
-}
-
 export type PropChanges = Record<string | symbol, PropChange>;
+
+export interface OnPropChanges {
+  onPropChanges(changes: PropChanges): void;
+}
 
 export function readPropertyDefs(c: any): Record<string | symbol, {}> {
   return c.properties || c.prototype.properties || {};
-}
-
-/**
- * Mixin that applies an prop change to a base class
- */
-export function WithProps<T extends new (...args: any[]) => {}>(Base: T) {
-  return class extends Base {
-    $$propChanges: Map<string | symbol, PropChange> = new Map();
-    $$propChange: Promise<void> | null = null;
-
-    onPropChanges(_: PropChanges) {}
-
-    /**
-     * Marks a property as changed
-     * onPropChanges is called after all prop changes are defined.
-     * This batches onPropChanges calls.
-     */
-    definePropChange(propChange: PropChange): Promise<void> {
-      this.$$propChanges.set(propChange.key, propChange);
-
-      if (!this.$$propChange) {
-        // If there is no previous change defined set it up
-        this.$$propChange = Promise.resolve().then(() => {
-          // run onPropChanges here. This makes sure we capture all changes
-          this.onPropChanges(Object.fromEntries(this.$$propChanges));
-
-          // reset for next time
-          this.$$propChanges.clear();
-          this.$$propChange = null;
-        });
-      }
-
-      return this.$$propChange;
-    }
-  };
 }
 
 export function propChanges() {
