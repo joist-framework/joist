73735038df7047e435b77b5adb82048e503e20ff
deebloo <dannybluedesign@gmail.com>
2021-11-25 19:15:20 -0500

    add event target test

--- packages/observable/lib/observable.test.ts
+++ packages/observable/lib/observable.test.ts
@@ -46,4 +46,31 @@ describe('property', () => {
     el.counter = el.counter + 1;
     el.counter = el.counter + 1;
   });
+
+  it('should detect and batch property updates with an EventEmitter', (done) => {
+    @observable()
+    class State extends EventTarget implements OnChange {
+      @observe() counter = 0;
+
+      onChange(val: Changes) {
+        this.dispatchEvent(new CustomEvent('statechange', { detail: val }));
+      }
+    }
+
+    const state = new State();
+
+    state.addEventListener('statechange', (e) => {
+      const evt = e as CustomEvent<Changes>;
+
+      expect(evt.detail.counter.value).to.equal(5);
+
+      done();
+    });
+
+    state.counter = state.counter + 1;
+    state.counter = state.counter + 1;
+    state.counter = state.counter + 1;
+    state.counter = state.counter + 1;
+    state.counter = state.counter + 1;
+  });
 });
