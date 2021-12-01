f3372e7b21374d5bb832ce7c3bd17e7f012089fe
dblue <dblue@deloitte.com>
2021-12-01 15:39:04 -0500

    test

--- packages/di/lib/service.ts
+++ packages/di/lib/service.ts
@@ -1,6 +1,6 @@
 import { ProviderToken } from '../lib/provider';
 
-export function service(provider: ProviderToken<any>) {
+export function service(provider: ProviderToken<any>): any {
   Object.defineProperty(provider, 'provideInRoot', { value: true });
 
   return provider;
