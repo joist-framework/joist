260f6a6ba7cfdd75e3784f3b0e0806874efb5834
dblue <dannybluedesign@gmail.com>
2022-04-04 12:44:18 -0400

    stuff

--- packages/observable/lib/attribute-parsers.ts
+++ packages/observable/lib/attribute-parsers.ts
@@ -5,26 +5,6 @@ export interface AttributeParser<T> {
 
 export type AttributeParsers = Record<string, AttributeParser<unknown>>;
 
-export const number: AttributeParser<number> = {
-  read: Number,
-  write: String,
-};
-
-export const date: AttributeParser<Date> = {
-  read: (val) => new Date(val),
-  write: String,
-};
-
-export const json: AttributeParser<Object> = {
-  read: JSON.parse,
-  write: JSON.stringify,
-};
-
-export const bool: AttributeParser<boolean> = {
-  read: (val) => val === 'true',
-  write: String,
-};
-
 export function defaultParser(): AttributeParser<boolean | string> {
   return {
     read(val: string) {
