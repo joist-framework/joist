bed442e318bbf770dec164fb1eb8204fa646b5b9
deebloo <dannybluedesign@gmail.com>
2022-08-26 14:03:28 -0400

    stuff

---
+++ packages/eslint/src/class-declaration.ts
@@ -0,0 +1,63 @@
+import { TSESTree } from '@typescript-eslint/typescript-estree';
+
+export function ClassDeclaration(node: TSESTree.ClassDeclaration) {
+  const injectedProp = node.body.body.find(
+    (body) =>
+      body.type === TSESTree.AST_NODE_TYPES.PropertyDefinition &&
+      body.key.type === TSESTree.AST_NODE_TYPES.Identifier &&
+      body.key.name === 'inject'
+  );
+
+  const injectedConstructor = node.body.body.find(
+    (body) =>
+      body.type === TSESTree.AST_NODE_TYPES.MethodDefinition &&
+      body.key.type === TSESTree.AST_NODE_TYPES.Identifier &&
+      body.key.name === 'constructor'
+  );
+
+  const injectedServices = injectedProp
+    ? findInjectedServices(injectedProp as TSESTree.PropertyDefinition)
+    : [];
+
+  const injectedArgs = injectedConstructor
+    ? findInjectedArgs(injectedConstructor as TSESTree.MethodDefinition)
+    : [];
+
+  for (let i = 0; i < injectedServices.length; i++) {}
+}
+
+function findInjectedServices(element: TSESTree.PropertyDefinition): Array<string | null> {
+  if (element.value?.type === TSESTree.AST_NODE_TYPES.ArrayExpression) {
+    return element.value.elements.map((element) => {
+      if (element.type === TSESTree.AST_NODE_TYPES.Identifier) {
+        return element.name;
+      }
+
+      return null;
+    });
+  }
+
+  return [];
+}
+
+function findInjectedArgs(element: TSESTree.MethodDefinition) {
+  if (element.value.type === TSESTree.AST_NODE_TYPES.FunctionExpression) {
+    return element.value.params.map((param) => {
+      if (param.type === TSESTree.AST_NODE_TYPES.Identifier && param.typeAnnotation) {
+        if (param.typeAnnotation.typeAnnotation.type === TSESTree.AST_NODE_TYPES.TSTypeReference) {
+          if (
+            param.typeAnnotation.typeAnnotation.typeName.type === TSESTree.AST_NODE_TYPES.Identifier
+          ) {
+            if (param.typeAnnotation.typeAnnotation.typeName.name === 'Injected') {
+              return param.typeAnnotation.typeAnnotation;
+            }
+          }
+        }
+      }
+
+      return null;
+    });
+  }
+
+  return [];
+}
