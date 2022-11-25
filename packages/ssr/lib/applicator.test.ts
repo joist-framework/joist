43e1572e81b2c489f7cb9771efc1b8f2b16315e5
Danny Blue <dblue@deloitte.com>
2022-11-25 16:46:00 -0500

    add test for recursive application

--- packages/ssr/lib/applicator.test.ts
+++ packages/ssr/lib/applicator.test.ts
@@ -4,7 +4,7 @@ import { Applicator } from './applicator.js';
 import { NoopTemplateCache } from './template-cache.js';
 import { TemplateLoader } from './template-loader.js';
 
-test('should word', async (t) => {
+test('should apply declarative shadow dom to specified elements', async (t) => {
   class MockTemplateLoader implements TemplateLoader {
     loadCSS(tag: string): Promise<string | null> {
       return Promise.resolve(`:host { content: 'css for ${tag}' }`);
@@ -57,6 +57,72 @@ test('should word', async (t) => {
                 <div>html for mock-footer</div>
                 </template>
             </mock-footer>
+            
+            <script>
+            (function attachShadowRoots(root) {
+            root.querySelectorAll("template[shadowroot]").forEach(template => {
+                const mode = template.getAttribute("shadowroot");
+                const shadowRoot = template.parentNode.attachShadow({ mode });
+                shadowRoot.appendChild(template.content);
+                template.remove();
+                attachShadowRoots(shadowRoot);
+            });
+            })(document);
+            </script>
+        </body>
+    </html>
+  `)
+  );
+});
+
+test('should apply declarative shadow dom recursively', async (t) => {
+  class MockTemplateLoader implements TemplateLoader {
+    async loadCSS(tag: string): Promise<string | null> {
+      return `:host { content: 'css for ${tag}' }`;
+    }
+
+    async loadHTML(tag: string): Promise<string | null> {
+      switch (tag) {
+        case 'mock-foo':
+          return `<mock-bar></mock-bar>`;
+
+        case 'mock-bar':
+          return `<mock-baz></mock-baz>`;
+      }
+
+      return `<div>html for ${tag}</div>`;
+    }
+  }
+
+  const applicator = new Applicator(new NoopTemplateCache(), new MockTemplateLoader());
+
+  const document = `<mock-foo></mock-foo>`;
+
+  const res = await applicator.apply(document, ['mock-foo', 'mock-bar', 'mock-baz']);
+
+  t.is(
+    trim(res),
+    trim(`
+    <html>
+        <head></head>
+
+        <body>
+            <mock-foo>
+                <template shadowroot="open">
+                    <style>:host { content: 'css for mock-foo'}</style>
+                    <mock-bar>
+                        <template shadowroot="open">
+                            <style>:host { content: 'css for mock-bar'}</style>
+                            <mock-baz>
+                                <template shadowroot="open">
+                                    <style>:host { content: 'css for mock-baz'}</style>
+                                    <div>html for mock-baz</div>
+                                </template>
+                            </mock-baz>
+                        </template>
+                    </mock-bar>
+                </template>
+            </mock-foo>
             
             <script>
             (function attachShadowRoots(root) {
