d67001e78415ac21d9fa63b2d902ed6ee51efb23
deebloo <dannybluedesign@gmail.com>
2022-12-30 16:13:56 -0500

    update exports

--- integration/todo-own/src/todo-form.element.ts
+++ integration/todo-own/src/todo-form.element.ts
@@ -1,68 +1,68 @@
 import { Injected, injectable } from '@joist/di';
-import { shadow, css, html } from '@joist/shadow';
-import { UpgradableElement } from '@joist/observable';
+import { shadow, css, html, ShadowOpts } from '@joist/shadow';
 
 import { TodoService, Todo } from './services/todo.service.js';
 
-export const styles = css`
-  :host {
-    display: block;
-    background: #fff;
-  }
+export const opts: ShadowOpts = {
+  styles: css`
+    :host {
+      display: block;
+      background: #fff;
+    }
 
-  input {
-    box-sizing: border-box;
-    display: block;
-    padding: 1rem;
-    border: none;
-    background: rgba(0, 0, 0, 0.003);
-    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
-    margin: 0;
-    width: 100%;
-    font-size: 24px;
-    line-height: 1.4em;
-    border: 0;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-  }
+    input {
+      box-sizing: border-box;
+      display: block;
+      padding: 1rem;
+      border: none;
+      background: rgba(0, 0, 0, 0.003);
+      box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+      margin: 0;
+      width: 100%;
+      font-size: 24px;
+      line-height: 1.4em;
+      border: 0;
+      -webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+    }
 
-  :focus {
-    outline: none;
-  }
+    :focus {
+      outline: none;
+    }
 
-  ::-webkit-input-placeholder {
-    font-style: italic;
-    font-weight: 300;
-    color: #e6e6e6;
-  }
+    ::-webkit-input-placeholder {
+      font-style: italic;
+      font-weight: 300;
+      color: #e6e6e6;
+    }
 
-  ::-moz-placeholder {
-    font-style: italic;
-    font-weight: 300;
-    color: #e6e6e6;
-  }
+    ::-moz-placeholder {
+      font-style: italic;
+      font-weight: 300;
+      color: #e6e6e6;
+    }
 
-  ::input-placeholder {
-    font-style: italic;
-    font-weight: 300;
-    color: #e6e6e6;
-  }
-`;
-
-export const template = html`
-  <form>
-    <input
-      id="input"
-      name="todo"
-      placeholder="What needs to be done?"
-      autocomplete="off"
-      autofocus
-    />
-  </form>
-`;
+    ::input-placeholder {
+      font-style: italic;
+      font-weight: 300;
+      color: #e6e6e6;
+    }
+  `,
+  template: html`
+    <form>
+      <input
+        id="input"
+        name="todo"
+        placeholder="What needs to be done?"
+        autocomplete="off"
+        autofocus
+      />
+    </form>
+  `,
+};
 
 @injectable
-export class TodoFormElement extends UpgradableElement {
+export class TodoFormElement extends HTMLElement {
   static inject = [TodoService];
 
   #shadow: ShadowRoot;
@@ -73,7 +73,7 @@ export class TodoFormElement extends UpgradableElement {
     super();
 
     this.#todos = todos;
-    this.#shadow = shadow(this, { template, styles });
+    this.#shadow = shadow(this, opts);
     this.#input = this.#shadow.querySelector<HTMLInputElement>('#input')!;
 
     this.#shadow.addEventListener('submit', this.#onSubmit.bind(this));
--- packages/shadow/lib.ts
+++ packages/shadow/lib.ts
@@ -1,3 +1,3 @@
 export { Result } from './lib/result.js';
 export { css, html } from './lib/tags.js';
-export { shadow } from './lib/shadow.js';
+export { shadow, ShadowOpts } from './lib/shadow.js';
