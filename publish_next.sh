0cd1bcdf6760f37dba8fd71a4b6bb4d6e0884a8e
deebloo <dannybluedesign@gmail.com>
2023-05-30 15:35:43 -0400

    scripting

--- publish_next.sh
+++ publish_next.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+
+npm version prerelease --preid next
 npm version prerelease --workspaces --preid next
 npm publish --workspaces --tag next
-git add -A && git commit -m"Update Next Version"
+git add -A && git commit -m"$(node -p "require('./package.json').version")"
