7f6437aed1ffb063737f30a206b88b5febc4eedd
deebloo <dannybluedesign@gmail.com>
2023-09-19 11:06:41 -0400

    allow user to define global environment

--- packages/di/src/lib.ts
+++ packages/di/src/lib.ts
@@ -2,3 +2,4 @@ export { Injector, Injectable } from './lib/injector.js';
 export { Provider, ProviderToken } from './lib/provider.js';
 export { injectable } from './lib/injectable.js';
 export { inject, Injected } from './lib/inject.js';
+export { defineEnvironment, clearEnvironment } from './lib/environment.js';
--- packages/di/src/lib/environment.ts
+++ packages/di/src/lib/environment.ts
@@ -1,9 +1,14 @@
 import { Injector } from './injector.js';
+import { Provider } from './provider.js';
 
 const rootInjector = new Injector();
 
 export function environment(): Injector {
   return rootInjector;
+}
+
+export function defineEnvironment(providers: Provider<any>[]) {
+  environment().providers = providers;
 }
 
 export function clearEnvironment(): void {
