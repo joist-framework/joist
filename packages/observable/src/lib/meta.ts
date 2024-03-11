dcc6e4a615ab6915c469ab141da5cb741ab4d9fe
deebloo <dannybluedesign@gmail.com>
2024-03-11 14:08:22 -0400

    sill mistakes

--- packages/observable/src/lib/meta.ts
+++ packages/observable/src/lib/meta.ts
@@ -11,7 +11,7 @@ export abstract class MetadataStore<Metadata> {
     if (!data) {
       data = this.init();
 
-      this.#data.set(value, this.init());
+      this.#data.set(value, data);
     }
 
     return data;
