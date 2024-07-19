123030f790b0c1d7a9bec73a857bfd38a433435b
deebloo <deebloo@proton.me>
2024-07-19 15:05:29 -0400

    add new test

--- packages/di/src/lib/lifecycle.test.ts
+++ packages/di/src/lib/lifecycle.test.ts
@@ -2,6 +2,7 @@ import { expect } from '@open-wc/testing';
 import { Injector } from './injector.js';
 import { LifeCycle } from './lifecycle.js';
 import { injectable } from './injectable';
+import { inject } from './inject.js';
 
 describe('LifeCycle', () => {
   it('should call onInit and onInject when a service is first created', () => {
@@ -51,6 +52,39 @@ describe('LifeCycle', () => {
     }
 
     i.inject(MyService);
+    i.inject(MyService);
+
+    expect(res).to.deep.equal({
+      onInit: 1,
+      onInject: 2
+    });
+  });
+
+  it('should call onInject and on init when injected from another service', () => {
+    const i = new Injector();
+
+    const res = {
+      onInit: 0,
+      onInject: 0
+    };
+
+    @injectable
+    class MyService {
+      [LifeCycle.onInit]() {
+        res.onInit++;
+      }
+
+      [LifeCycle.onInject]() {
+        res.onInject++;
+      }
+    }
+
+    @injectable
+    class MyApp {
+      service = inject(MyService);
+    }
+
+    i.inject(MyApp).service();
     i.inject(MyService);
 
     expect(res).to.deep.equal({
