0fc6382ebbd9eb63fc90052b56a0d176c0365631
deebloo <deebloo@proton.me>
2024-07-21 21:08:33 -0400

    remove undocumented code

--- packages/di/src/lib.ts
+++ packages/di/src/lib.ts
@@ -1,5 +1,5 @@
 export { Injector } from './lib/injector.js';
-export { Provider, ConstructableToken, StaticToken, token } from './lib/provider.js';
+export { Provider, ConstructableToken, StaticToken } from './lib/provider.js';
 export { injectable, INJECTABLE_MAP } from './lib/injectable.js';
 export { inject, Injected } from './lib/inject.js';
 export { defineEnvironment, clearEnvironment } from './lib/environment.js';
