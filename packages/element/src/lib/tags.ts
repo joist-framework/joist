e198da7509ed7e8705774a38281708b10c3863f0
deebloo <deebloo@proton.me>
2024-07-31 11:32:13 -0400

    template cacheing no longer needed

--- packages/element/src/lib/tags.ts
+++ packages/element/src/lib/tags.ts
@@ -1,34 +1,9 @@
-/**
- * NOTE: TemplateStringsArray can be used to cache via a WeakMap.
- *
- * function html(strs: TemplateStringsArray) {
- *   return strs
- * }
- *
- * class Foo {
- *   hello = html`world`;
- * }
- *
- * // these will be the same instance of TemplateStringsArray
- * new Foo().hello === new Foo().hello
- */
-
 import { JoistShadowResult } from './result.js';
-
-export const htmlTemplateCache = new WeakMap<TemplateStringsArray, HTMLTemplateElement>();
 
 export class HTMLResult extends JoistShadowResult {
   setup(root: ShadowRoot): void {
-    let template: HTMLTemplateElement;
-
-    if (htmlTemplateCache.has(this.strings)) {
-      template = htmlTemplateCache.get(this.strings) as HTMLTemplateElement;
-    } else {
-      template = document.createElement('template');
-
-      template.innerHTML = concat(this.strings);
-      htmlTemplateCache.set(this.strings, template);
-    }
+    let template = document.createElement('template');
+    template.innerHTML = concat(this.strings);
 
     root.append(template.content.cloneNode(true));
   }
@@ -38,19 +13,10 @@ export function html(strings: TemplateStringsArray, ...values: any[]): HTMLResul
   return new HTMLResult(strings, ...values);
 }
 
-export const styleSheetCache = new WeakMap<TemplateStringsArray, CSSStyleSheet>();
-
 export class CSSResult extends JoistShadowResult {
   setup(root: ShadowRoot): void {
-    let sheet: CSSStyleSheet;
-
-    if (styleSheetCache.has(this.strings)) {
-      sheet = styleSheetCache.get(this.strings) as CSSStyleSheet;
-    } else {
-      sheet = new CSSStyleSheet();
-
-      sheet.replaceSync(concat(this.strings));
-    }
+    let sheet = new CSSStyleSheet();
+    sheet.replaceSync(concat(this.strings));
 
     root.adoptedStyleSheets = [...root.adoptedStyleSheets, sheet];
   }
