a49dca774e663ff5c37159ec8d71b966bf9c1f80
deebloo <deebloo@proton.me>
2024-08-05 22:11:45 -0400

    add loading element

---
+++ integration/hacker-news/src/loading/loading.ts
@@ -0,0 +1,28 @@
+import { element, css } from '@joist/element';
+
+@element({
+  tagName: 'hn-loading',
+  shadow: [
+    css`
+      :host {
+        border: 16px solid #f3f3f3; /* Light grey */
+        border-top: 16px solid #3498db; /* Blue */
+        border-radius: 50%;
+        width: 100px;
+        height: 100px;
+        animation: spin 2s linear infinite;
+        display: block;
+      }
+
+      @keyframes spin {
+        0% {
+          transform: rotate(0deg);
+        }
+        100% {
+          transform: rotate(360deg);
+        }
+      }
+    `
+  ]
+})
+export class HnLoadingElement extends HTMLElement {}
