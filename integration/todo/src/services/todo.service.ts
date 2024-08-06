ee6a876cdaa3412d9e3e869cd9f47500d31fcc6a
deebloo <deebloo@proton.me>
2024-08-05 22:34:01 -0400

    test

--- integration/todo/src/services/todo.service.ts
+++ integration/todo/src/services/todo.service.ts
@@ -43,14 +43,18 @@ export class TodoSyncEvent extends Event {
 
 @injectable()
 export class TodoService extends EventTarget {
-  @observe accessor #todos: Todo[] = [];
-  @observe accessor #initialized = false;
+  @observe()
+  accessor #todos: Todo[] = [];
+
+  @observe()
+  accessor #initialized = false;
 
   totalActive = 0;
 
   #store = inject(AppStorage);
 
-  @effect syncTodosToStorage() {
+  @effect()
+  syncTodosToStorage() {
     this.#store().saveJSON('joist_todo', this.#todos);
 
     this.totalActive = this.#todos.reduce(
