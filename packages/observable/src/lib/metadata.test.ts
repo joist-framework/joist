eb7e200bce1c59050b970464288b988d9ad0c8ca
deebloo <deebloo@proton.me>
2024-08-06 15:57:20 -0400

    update observable to use vitest

--- packages/observable/src/lib/metadata.test.ts
+++ packages/observable/src/lib/metadata.test.ts
@@ -1,21 +1,16 @@
-import { expect } from '@open-wc/testing';
+import { test, assert } from 'vitest';
 import { ObservableInstanceMetaDataStore } from './metadata.js';
 
-describe('meta:meta', () => {
-  it('should return default metadata', () => {
-    const key = {};
-    const data = new ObservableInstanceMetaDataStore().read(key);
+test('should return default metadata', () => {
+  const key = {};
+  const data = new ObservableInstanceMetaDataStore().read(key);
 
-    expect(data).to.deep.equal({
-      changes: new Set(),
-      scheduler: null
-    });
-  });
+  assert.deepEqual(data, { changes: new Set<string | symbol>(), scheduler: null });
+});
 
-  it('should return the same metadata object after init', () => {
-    const key = {};
-    const data = new ObservableInstanceMetaDataStore();
+test('should return the same metadata object after init', () => {
+  const key = {};
+  const data = new ObservableInstanceMetaDataStore();
 
-    expect(data.read(key)).to.equal(data.read(key));
-  });
+  assert.equal(data.read(key), data.read(key));
 });
