950e0c1d6eeadc9e66b97f0e507b84e6e77bc55e
deebloo <deebloo@proton.me>
2024-08-20 16:17:23 -0400

    stuff and things

--- integration/hacker-news/vite.config.mjs
+++ integration/hacker-news/vite.config.mjs
@@ -1,5 +1,6 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import { minifyHTMLLiterals } from 'minify-html-literals';
 
 export default defineConfig({
   build: {
@@ -8,7 +9,13 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         static: resolve(__dirname, 'static.html'),
         vanilla: resolve(__dirname, 'vanilla.html')
-      }
+      },
+      plugins: [
+        {
+          name: 'minify-html-literals',
+          transform: (code) => minifyHTMLLiterals(code)?.code
+        }
+      ]
     }
   }
 });
