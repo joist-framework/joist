3a233664c0508583eb6cf7445c61c8a810bd71b9
deebloo <dannybluedesign@gmail.com>
2024-10-06 21:24:18 -0400

    add vite plugin wrapper for ssr

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,37 +1,20 @@
 import { Applicator, NoopTemplateCache, FileSysTemplateLoader } from '@joist/ssr';
+import joist from '@joist/plugin-vite';
 import { defineConfig } from 'vite';
-
-const applicator = new Applicator(
-  new NoopTemplateCache(),
-  new FileSysTemplateLoader(
-    (tag) => `./src/elements/${tag}/${tag}.html`,
-    (tag) => `./src/elements/${tag}/${tag}.css`
-  )
-);
 
 export default defineConfig({
   esbuild: {
     target: 'es2020'
   },
   plugins: [
-    {
-      name: 'Web Component SSR',
-      transformIndexHtml: {
-        order: 'pre',
-        handler(html) {
-          return applicator.apply(html, ['joist-header', 'joist-nav', 'joist-main']);
-        }
-      },
-      handleHotUpdate({ file, server }) {
-        if (file.includes('elements') && (file.endsWith('.html') || file.endsWith('.css'))) {
-          console.log(`${file} updated...`);
-
-          server.ws.send({
-            type: 'full-reload',
-            path: '*'
-          });
-        }
-      }
-    }
+    joist(
+      new Applicator(
+        new NoopTemplateCache(),
+        new FileSysTemplateLoader(
+          (tag) => `./src/elements/${tag}/${tag}.html`,
+          (tag) => `./src/elements/${tag}/${tag}.css`
+        )
+      )
+    )
   ]
 });
