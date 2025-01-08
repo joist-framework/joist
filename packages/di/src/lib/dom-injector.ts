81cce8e098169a114763a872ed24db2a7042d955
deebloo <deebloo@proton.me>
2025-01-08 12:55:01 -0500

    update dom-injector to use the context protocol

--- packages/di/src/lib/dom-injector.ts
+++ packages/di/src/lib/dom-injector.ts
@@ -1,8 +1,19 @@
+import { ContextRequestEvent, INJECTOR_CTX } from './context.js';
 import { injectables, Injector } from './injector.js';
 
 export class DOMInjector extends Injector {
+  #contextCallback = (e: ContextRequestEvent<{ __context__: unknown }>) => {
+    if (e.context === INJECTOR_CTX) {
+      e.stopPropagation();
+
+      const event = e as ContextRequestEvent<typeof INJECTOR_CTX>;
+
+      event.callback(this);
+    }
+  };
+
   attach(root: HTMLElement): void {
-    injectables.set(root, this);
+    root.addEventListener('context-request', this.#contextCallback);
   }
 
   detach(root: HTMLElement): void {
