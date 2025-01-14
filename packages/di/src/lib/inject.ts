748a800e3c6866137b5b2ba45ee03e9d53583a3c
deebloo <deebloo@proton.me>
2025-01-14 12:26:53 -0500

    cleanup

--- packages/di/src/lib/inject.ts
+++ packages/di/src/lib/inject.ts
@@ -10,10 +10,8 @@ export function inject<This extends object, T>(
     const injector = injectables.get(this);
 
     if (injector === undefined) {
-      const name = this.constructor.name;
-
       throw new Error(
-        `${name} is either not injectable or a service is being called in the constructor. \n Either add the @injectable() to your class or use the @injected callback method.`,
+        `${this.constructor.name} is either not injectable or a service is being called in the constructor. \n Either add the @injectable() to your class or use the @injected callback method.`,
       );
     }
 
