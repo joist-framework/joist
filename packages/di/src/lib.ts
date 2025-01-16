1183df67d109737d8d987436076e48f2c40ea843
deebloo <deebloo@proton.me>
2025-01-16 13:18:21 -0500

    expose InjectionToken type from root

--- packages/di/src/lib.ts
+++ packages/di/src/lib.ts
@@ -1,5 +1,10 @@
 export { Injector } from "./lib/injector.js";
-export { Provider, ConstructableToken, StaticToken } from "./lib/provider.js";
+export {
+  Provider,
+  ConstructableToken,
+  StaticToken,
+  InjectionToken,
+} from "./lib/provider.js";
 export { injectable } from "./lib/injectable.js";
 export { inject, Injected } from "./lib/inject.js";
 export { injected, created } from "./lib/lifecycle.js";
