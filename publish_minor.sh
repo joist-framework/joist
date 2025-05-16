bc9469b2d86532555251bbd0403efefd21dee79f
deebloo <deebloo@proton.me>
2025-05-16 12:31:42 -0400

    tweak publish scripts

--- publish_minor.sh
+++ publish_minor.sh
@@ -2,5 +2,5 @@
 
 npm version minor
 npm version minor -w packages/
-npm publish -w packages/
+npm publish -w packages/ --tag latest
 git add -A && git commit -m"$(node -p "require('./package.json').version")"
--- publish_patch.sh
+++ publish_patch.sh
@@ -2,5 +2,5 @@
 
 npm version patch
 npm version patch -w packages/
-npm publish -w packages/
+npm publish -w packages/ --tag latest
 git add -A && git commit -m"$(node -p "require('./package.json').version")"
