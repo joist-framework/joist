5422cbfc598b5d095d2480fe9337a07b167d64b1
deebloo <deebloo@proton.me>
2025-06-11 10:24:38 -0400

    correctly register scope element

--- packages/templating/src/lib/define.ts
+++ packages/templating/src/lib/define.ts
@@ -3,3 +3,4 @@ import "./elements/for.element.js";
 import "./elements/if.element.js";
 import "./elements/bind.element.js";
 import "./elements/value.element.js";
+import "./elements/scope.element.js";
