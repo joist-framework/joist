36c664ce9730efadd0e022eea72d733995d4ee9a
deebloo <deebloo@proton.me>
2026-04-30 10:21:45 -0400

    fix(di): correctly export create function

--- packages/di/src/lib.ts
+++ packages/di/src/lib.ts
@@ -6,6 +6,6 @@ export {
   type InjectionToken,
 } from "./lib/provider.js";
 export { injectable } from "./lib/injectable.js";
-export { inject, injectAll, type Injected } from "./lib/inject.js";
+export { inject, create, injectAll, type Injected } from "./lib/inject.js";
 export { injected, created } from "./lib/lifecycle.js";
 export { DOMInjector } from "./lib/dom/dom-injector.js";
